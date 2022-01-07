3ee3a094d3166b00579ccef9ad55c274c10fda37
KimTaeyoun <kimty1121@gmail.com>
2022-01-08 00:02:50 +0900

    백준 브루트 포스 공부중

---
+++ 브루트_포스/2798/2231/test.java
@@ -0,0 +1,17 @@
+public class test {
+    public static void main(String[] args) {
+        int a = 216 / 10; //21
+        int b = 21 / 10; //2
+        int c = 2 / 10; // 0?
+
+        int d = 216 % 10; //6
+        int e = 21 % 10; //1
+        int f = 2 % 10; // 2?
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
